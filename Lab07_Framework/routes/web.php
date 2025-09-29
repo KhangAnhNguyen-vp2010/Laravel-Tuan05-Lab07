173a3e8d3f30fc745fbb18a2966bf36b5a63e63d
Anh Khang <taicuoi664@gmail.com>
2025-09-29 23:22:05 +0700

    Push Bai Tap 02

--- Lab07_Framework/routes/web.php
+++ Lab07_Framework/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -20,3 +21,5 @@ Route::get('/articles', fn() => 'Quản trị bài viết')
 ->name('admin.articles.index');
 
 });
+
+Route::resource('articles', ArticleController::class);
